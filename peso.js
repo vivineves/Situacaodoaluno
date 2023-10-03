895a7baf1f66d20f642e0cacdbfd3f74e18801fd
vivineves <105659877+vivineves@users.noreply.github.com>
2023-10-03 12:28:26 -0300

    Add files via upload

---
+++ peso.js
@@ -0,0 +1,27 @@
+//cria referencial ao form
+const form = document.querySelector ("form")
+const resp = document.querySelector ("fh3")
+
+//ouvinte
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const nome = form.inNome.value
+    const masculino = form.inMasculino.checked
+    const altura = Number (form.inAltura.value)
+    
+//calculando o peso ideal
+let peso
+if (masculino) {
+    peso = 22 * Math.pow(altura, 2)   
+} else {
+    peso = 21 * Math.pow(altura, 2)
+}
+//apresenta a resposta
+resp.innerText = `${nome} seu peso ideal é: ${peso.toFixed(3)}kg`
+})
+
+//segundo ouvinde. limpa o conteúdo de h3
+
+form.addEventListener("reset", (e) => {
+    resp.innerText = " " //limpa o contúdo de h3
+})
