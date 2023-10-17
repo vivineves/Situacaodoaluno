eb4b3867a9af0834960bee000542d1091dee1794
vivineves <105659877+vivineves@users.noreply.github.com>
2023-10-17 11:52:18 -0300

    Update velocidade.js

--- velocidade.js
+++ velocidade.js
@@ -4,6 +4,7 @@ const resp = document.querySelector("h3")
 //ouvinte
 frm = addEventListener("submit", (e) =>{
     e.preventDefault()
+    
     const permitida = Number(frm.inPermitida.value)
     const condutor = Number(frm.inCondutor.value)
     let leve = permitida + (permitida * 0.2)
