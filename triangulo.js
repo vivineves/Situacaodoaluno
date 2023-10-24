d32b7d924cf49ae0bf6fdaccd0c5f36bf5164b83
vivineves <105659877+vivineves@users.noreply.github.com>
2023-10-24 11:57:58 -0300

    Update triangulo.js

--- triangulo.js
+++ triangulo.js
@@ -1,27 +1,29 @@
-const frm = document.querySelector("form")
+const form = document.querySelector("form")
 const resp = document.querySelector("#outResp")
-const resp = document.querySelector("#outResp1")
+const resp1 = document.querySelector("#outResp1")
 
 //ouvinte
-frm = addEventListener("submit", (e) =>{
+form = addEventListener("submit", (e)=>{
     e.preventDefault()
     
-    const ladoA = Number(frm.inLadoA.value)
-    const ladoB = Number(frm.inLadoB.value)
-    const ladoC = Number(frm.inLadoC.value)
+    const ladoA = Number(form.inLadoA.value)
+    const ladoB = Number(form.inLadoB.value)
+    const ladoC = Number(form.inLadoC.value)
 
-    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB) ||){
-      
+    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
+      alert("Essas medidas não podem formar um triângulo. Um dos lados não pode ser maior qua a soma dos outros dois.")
         form.inLadoA.focus()
         form.inLadoB.focus()
         form.inLadoC.focus()
         return
+    }
 
-       if(ladoA ==ladoB &&)
-        resp.innerText = `Situação: Sem multa`
-    } else if(condutor <= leve){
-        resp.innerText = `Situação: Multa leve`
+    resp.innerText = `Lados podem formar um triângulo`
+    if(ladoA == ladoB && ladoA == ladoC){
+        resp1.innerText = `Tipo: Equilátero`
+    } else if((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)) {
+        resp1.innerText = `Tipo: Isóceles`
     } else {
-        resp.innerText = `Situação: Multa grave`
+        resp1.innerText = `Tipo: Escaleno`
     }
 })
